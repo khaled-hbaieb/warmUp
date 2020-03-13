b1e85078222ac6a9f5412f93c76f24e198e7148a
khaled-hbaieb <khaledhbaieb@yahoo.fr>
2020-03-13 10:05:59 +0100

    All the questions have been solved!

--- warmUp1.js
+++ warmUp1.js
@@ -4,3 +4,29 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+
+//1
+function combineName(firstName, lastName){
+	return firstName +' '+ lastName;
+}
+//2
+function multipleOrNot(num1,num2){
+	if((num1 % num2) === 0) {
+		return num1 +' is a multiple of ' + num2
+	}
+	return num1 +' is not a multiple of ' + num2
+}
+//3
+function average(array){
+	var result=0
+	for (var i=0; i<array.length; i++){
+		result+=array[i]
+	}
+	return result/array.length
+}
+
+//4
+function ageInSeconds(age){
+	return age*12*30*24*60*60
+}
