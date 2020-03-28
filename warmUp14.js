70bf25194670a76cf0439b3c3c2beadbf895d576
khaled-hbaieb <khaledhbaieb@yahoo.fr>
2020-03-28 10:14:20 +0100

    still working on it

--- warmUp14.js
+++ warmUp14.js
@@ -7,3 +7,18 @@
 // array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 // array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 // array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+function isSubset (arr1, arr2) {
+//debugger;
+	for(var i = 0; i < arr1.length; i++){
+		for(var j = 0; j < arr2.length; j++){
+			if(arr1[i] === arr2[j]){
+				arr2.shift(arr2[j]);
+			}
+		}
+    }
+			if (arr2.length === 0) {
+				return true
+			}
+				return false
+			}
